6c7b736d4ce5a62d91b62bfd457c2a03861b6c62
kasu naga veera venkata srirama anudeep <90023127+anudee796@users.noreply.github.com>
2021-11-19 13:18:23 +0530

    Create binary tree implementation.cpp

---
+++ binary tree/binary tree implementation.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node* create();
+struct node{ //userdefined datatype
+    int data;
+    struct node *left;
+    struct node *right;
+}
+void main(){
+    struct node *root; //userdefined datatype variable
+    root=create();
+    //return 0;
+}
+
+
+struct node* create(){
+    int x;
+    struct node *newnode; //userdefined datatype var
+    newnode=(struct node *)malloc(sizeof(struct node));
+    printf("enter the data\n");
+    scanf("%d",&x);
+    if(x==-1){
+        return 0;
+    }
+    printf("enter the left child of %d",x);
+    newnode->left=create();
+    printf("enter the left child of %d",x);
+    newnode->right=create();
+    return newnode;
+}
